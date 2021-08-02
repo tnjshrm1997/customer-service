8895bee7561bbce999fd63684793ae13668f9004
Ashim Sharma <tnjshrm1997@gmail.com>
2021-08-03 02:02:07 +0530

    Added entity classes for new version

--- src/main/java/com/mindstix/microservices/foundation/customerservice/entity/CustomerInformation.java
+++ src/main/java/com/mindstix/microservices/foundation/customerservice/entity/CustomerInformation.java
@@ -3,12 +3,10 @@ package com.mindstix.microservices.foundation.customerservice.entity;
 
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.data.annotation.CreatedDate;
 
 import javax.validation.constraints.Email;
 import javax.persistence.*;
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -21,14 +19,15 @@ public class CustomerInformation {
         @GeneratedValue(strategy = GenerationType.AUTO)
         @Column(updatable = false)
         private Integer pkCustomerId;
-        @Column(length = 255, columnDefinition = "varchar", nullable = false)
+        @Column(columnDefinition = "varchar", nullable = false)
         private String customerName;
         @Email
-        @Column(length = 255, columnDefinition = "varchar",unique = true,nullable = false)
+        @Column(columnDefinition = "varchar",unique = true,nullable = false)
         private String email;
         @Column(length = 10, columnDefinition = "varchar",unique = true,nullable = false)
         private String contact;
         @CreationTimestamp
+        @Column(columnDefinition = "date")
         private Timestamp createTime;
         @OneToOne(fetch = FetchType.LAZY,
                 cascade = CascadeType.ALL,
