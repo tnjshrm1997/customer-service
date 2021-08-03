2ff01a918844c3228f37f042352e632ea3aba664
Ashim Sharma <tnjshrm1997@gmail.com>
2021-08-04 02:23:07 +0530

    updated exception handling classes

--- src/main/java/com/mindstix/microservices/foundation/customerservice/exception/CustomerExceptionMessage.java
+++ src/main/java/com/mindstix/microservices/foundation/customerservice/exception/CustomerExceptionMessage.java
@@ -1,6 +1,7 @@
 package com.mindstix.microservices.foundation.customerservice.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class CustomerExceptionMessage {
     private final String message;
     private final HttpStatus httpStatus;
--- src/main/java/com/mindstix/microservices/foundation/customerservice/exception/CustomerServiceExceptionHandler.java
+++ src/main/java/com/mindstix/microservices/foundation/customerservice/exception/CustomerServiceExceptionHandler.java
@@ -2,16 +2,29 @@ package com.mindstix.microservices.foundation.customerservice.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class CustomerServiceExceptionHandler {
 
-    private static final HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+    private static final HttpStatus HTTP_BADREQUEST = HttpStatus.BAD_REQUEST;
     @ExceptionHandler(value={CustomerServiceExceptions.class})
     public ResponseEntity<Object> handleCustomerServiceException(CustomerServiceExceptions e){
-        CustomerExceptionMessage exception =  new CustomerExceptionMessage(e.getMessage(), badRequest);
-        return new ResponseEntity<>(exception,badRequest);
+        CustomerExceptionMessage exception =  toExceptionMessageObject(e.getMessage(), HTTP_BADREQUEST);
+        return new ResponseEntity<>(exception,HTTP_BADREQUEST);
+    }
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<Object> validationErrors(BindException exception){
+        CustomerExceptionMessage transactionException= toExceptionMessageObject(exception.getAllErrors().toString(), HTTP_BADREQUEST);
+        return new ResponseEntity<>(transactionException,HTTP_BADREQUEST);
+    }
+    public static CustomerExceptionMessage toExceptionMessageObject(String message, HttpStatus status){
+        return CustomerExceptionMessage
+                .builder()
+                .message(message)
+                .httpStatus(status)
+                .build();
     }
 }
